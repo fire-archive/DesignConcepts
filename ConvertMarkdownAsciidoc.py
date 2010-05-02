34a17b74487e301879dc27102e37073088a69b7e
Ernest Lee <fire@chibifire.com>
2010-05-02 16:45:55 -0700

    Fixed smooshed characters breaking asciidoc compiling.

--- ConvertMarkdownAsciidoc.py
+++ ConvertMarkdownAsciidoc.py
@@ -1,5 +1,6 @@
 import re
 import sys
+import string
 
 expressions = [
         (re.compile(r"""\[([^\]]+)\]\(([^)]*)\)"""), r"""\2[\1]"""),
@@ -8,14 +9,16 @@ expressions = [
         (re.compile(r"""##"""), """=="""),
         (re.compile(r"""#"""), """="""),
 
-        (re.compile(r"""\*\*([^* ][^*]*)\*\*(?:\s|[[:punct:]])"""), r"""__\1__"""),
-		(re.compile(r"""\*\*([^* ][^*]*)\*\*"""), r"""__\1__ """),
+        # first transform **foo** -> __foo__ globally
+        (re.compile(r"""\*\*([^* ][^*]*)\*\*"""), r"""__\1__"""),
 
-        (re.compile(r"""\*([^* ][^*]*)\*(?:\s|[[:punct:]])"""), r"""_\1_"""),
+        # now deal with *foo* -> _foo_, remember to add a space when you hit *foo*bar
+        (re.compile(r"""\*([^* ][^*]*)\*(\s|[%s])""" % string.punctuation), r"""_\1_\2"""),
         (re.compile(r"""\*([^* ][^*]*)\*"""), r"""_\1_ """),
-        
-        (re.compile(r"""__([^* ][^*]*)__(?:\s|[[:punct:]])"""), r"""*\1*"""),
-        (re.compile(r"""__([^* ][^*]*)__"""), r"""*\1* """),
+
+        # now __foo__ -> *foo*, remember to add a space when you hit __foo__bar
+        (re.compile(r"""__([^_ ][^_]*)__(\s|[%s])""" % string.punctuation), r"""*\1*\2"""),
+        (re.compile(r"""__([^_ ][^_]*)__"""), r"""*\1* """),
 ]
 
 for l in sys.stdin.readlines():
